ca2c8737ef513c56623ffcbdb263612eb299f8d4
onemineus <sangeetbanerjee777@gmail.com>
2022-12-20 19:31:49 +0530

    comit 2

--- server.js
+++ server.js
@@ -7,4 +7,50 @@ mongoose.connect(
   "mongodb+srv://kalobiral:kalobiral@cluster0.aj0djdx.mongodb.net/?retryWrites=true&w=majority"
 );
 
+const mongoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  thought: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
+const mongoModel = mongoose.model("model1", mongoSchema);
+
+app.listen(5555);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5555",
+  })
+);
+
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  const model = new mongoModel({
+    title: req.body.title,
+    thought: req.body.thought,
+    phoneNumber: req.body.phoneNumber,
+  });
+  model.save();
+  console.log("data saved");
+});
+
+app.get("/read", async (req, res) => {
+  let gotData = await mongoModel.find({});
+  res.send(gotData);
+  console.log("Data sent successfully");
+});
